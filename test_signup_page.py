b54ce24f80a0c1a63204f15bfc740815fc3ff79a
strong <strong.tretyakov@gmail.com>
2024-02-20 10:54:36 +0300

    Исправлено дублирование кода в class TestAllFieldsEmpty

--- test_signup_page.py
+++ test_signup_page.py
@@ -1,76 +1,63 @@
-from pyshadow.main import Shadow
 import time
 import pytest
 
+from pyshadow.main import Shadow
 from selenium.common import JavascriptException
 
 
 class TestAllFieldsEmpty:
     url = "https://koshelek.ru/authorization/signup"
 
-    def test_username_alert_field_empty(self, browser):
-
-        browser.get(TestAllFieldsEmpty.url)
+    @staticmethod
+    def button_click(browser):
         shadow = Shadow(browser)
         shadow.set_implicit_wait(10)
         try:
             button_element = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div.k-btn-long-button > button')
+                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > '
+                'div:nth-child(2) > form > div > div.k-btn-long-button > button')
             button_element.click()
 
         except JavascriptException:
             pytest.fail('Элемент "button_element" не найден, тест не может быть выполнен')
 
-        time.sleep(2)
+    @staticmethod
+    def check_alert_text(browser, element_selector, expected_text):
+        shadow = Shadow(browser)
         try:
-            username_alert_text = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(1) > div > div > div.v-text-field__details > div > div > div > div > div > span')
+            alert_text = shadow.find_element(element_selector)
 
-            assert username_alert_text.text == "Поле не заполнено", "Текст сообщения ошибки не соответствует ожидаемому"
+            assert alert_text.text == expected_text, "Текст сообщения ошибки не соответствует ожидаемому"
 
         except JavascriptException:
-            pytest.fail('Элемент "username_alert_text" не найден, тест не может быть выполнен')
+            pytest.fail(f'Элемент "{element_selector}" не найден, тест не может быть выполнен')
+
+    def test_username_alert_field_empty(self, browser):
+        browser.get(TestAllFieldsEmpty.url)
+        self.button_click(browser)
+        time.sleep(2)
+        self.check_alert_text(browser,
+                              'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child('
+                              '2) > form > div > div:nth-child(1) > div > div > div.v-text-field__details > div > div '
+                              '> div > div > div > span',
+                              'Поле не заполнено')
 
     def test_email_alert_field_empty(self, browser):
         browser.get(TestAllFieldsEmpty.url)
-        shadow = Shadow(browser)
-        shadow.set_implicit_wait(10)
-        try:
-            button_element = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div.k-btn-long-button > button')
-            button_element.click()
-
-        except JavascriptException:
-            pytest.fail('Элемент "button_element" не найден, тест не может быть выполнен')
-
+        self.button_click(browser)
         time.sleep(2)
-        try:
-            email_alert_text = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(2) > div > div > div.v-text-field__details > div > div > div > div > div > span')
-
-            assert email_alert_text.text == "Поле не заполнено", "Текст сообщения ошибки не соответствует ожидаемому"
-
-        except JavascriptException:
-            pytest.fail('Элемент "email_alert_text" не найден, тест не может быть выполнен')
+        self.check_alert_text(browser,
+                              'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child('
+                              '2) > form > div > div:nth-child(2) > div > div > div.v-text-field__details > div > div '
+                              '> div > div > div > span',
+                              "Поле не заполнено")
 
     def test_password_alert_field_empty(self, browser):
         browser.get(TestAllFieldsEmpty.url)
-        shadow = Shadow(browser)
-        shadow.set_implicit_wait(10)
-        try:
-            button_element = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div.k-btn-long-button > button')
-            button_element.click()
-
-        except JavascriptException:
-            pytest.fail('Элемент "button_element" не найден, тест не может быть выполнен')
-
+        self.button_click(browser)
         time.sleep(2)
-        try:
-            password_alert_text = shadow.find_element(
-                'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child(2) > form > div > div:nth-child(3) > div > div > div > div > div.v-text-field__details > div > div > div > div > div > span')
-
-            assert password_alert_text.text == "Поле не заполнено", "Текст сообщения ошибки не соответствует ожидаемому"
-
-        except JavascriptException:
-            pytest.fail('Элемент "password_alert_text" не найден, тест не может быть выполнен')
+        self.check_alert_text(browser,
+                              'div.remoteApplication > div > div > div > div.css-grid.k-text-default > div:nth-child('
+                              '2) > form > div > div:nth-child(3) > div > div > div > div > div.v-text-field__details '
+                              '> div > div > div > div > div > span',
+                              "Поле не заполнено")
